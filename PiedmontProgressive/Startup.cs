a7bd865d8cf3458f727f3b865386a756469fa218
CoderB <noodoggy@gmail.com>
2016-12-02 12:07:21 -0500

    Add project files.

---
+++ PiedmontProgressive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PiedmontProgressive.Startup))]
+namespace PiedmontProgressive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
